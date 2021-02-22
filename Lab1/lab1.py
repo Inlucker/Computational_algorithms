80f0e5680ee6341986ade4d60f35f32916dea9ac
Arseny Pronin <arseny707@mail.ru>
2021-02-22 19:25:34 +0300

    added make_data func

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -8,3 +8,23 @@ table = [(0.00, 1.000000, -1.00000),
          (1.05,-0.552430, -1.86742)]
 
 print (table)
+
+def make_data(x, n):
+    rez = []
+    rez.append(table[0])
+    index = 0;
+    for i in range(len(table)):
+        if table[i][0] <= x:
+            rez[0] = table[i]
+            index = i
+
+    tmp = 1
+    for i in range(0, n - 1):
+        if (i % 2 == 0):
+            rez.append(table[index + tmp])
+        else:
+            rez.insert(0, table[index - tmp])
+            tmp += 1
+    return rez
+
+print(make_data(0.5, 4))
