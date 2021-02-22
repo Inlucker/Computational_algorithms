f44b96db49b3335f00fc134ccd928a29ce882a81
Arseny Pronin <arseny707@mail.ru>
2021-02-22 23:25:03 +0300

    completed Newton polinom (almost)

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -17,19 +17,19 @@ table2 = ([0.00, 1.0,   1],
 
 def make_data(x, n):
     rez = []
-    rez.append(table2[0])
+    rez.append(table[0])
     index = 0;
-    for i in range(len(table2)):
-        if table2[i][0] <= x:
-            rez[0] = table2[i]
+    for i in range(len(table)):
+        if table[i][0] <= x:
+            rez[0] = table[i]
             index = i
 
     tmp = 1
     for i in range(0, n - 1):
         if (i % 2 == 0):
-            rez.append(table2[index + tmp])
+            rez.append(table[index + tmp])
         else:
-            rez.insert(0, table2[index - tmp])
+            rez.insert(0, table[index - tmp])
             tmp += 1
     return rez
 
@@ -37,20 +37,34 @@ def get_dif(data, yi, yj, xi, xj):
     dif = (data[yi][1] - data[yj][1]) / (data[xi][0] - data[xj][0])
     return dif
 
-def calculate_difs(data, n):
-    rez = list(data)
+def calculate_polinom_Newton(data, n, x):
+    rez = data[0][1]
+    difs = list(data)
+    #print(difs)
+    #print(rez)
     for i in range(n - 1):
         for j in range(n - 1 - i):
-            rez[j + i][1] = get_dif(rez, j, j + 1, j, j + 1 + i)
+            difs[j][1] = get_dif(difs, j, j + 1, j, j + 1 + i)
+        tmp = difs[0][1]
+        for k in range (i + 1):
+            mn = x - difs[k][0]
+            tmp *= x - difs[k][0]
+        rez += tmp
+        #print("Difs = ", difs)
+        #print("Rez = ", rez)
+        #print("Tmp = ", tmp)
+
     return rez
 
 number = 5
 
-new_data = make_data(0.5, number)
+cur_x = 0.525
+
+new_data = make_data(cur_x, number)
 
 print(new_data)
 
-new_difs = calculate_difs(new_data, number)
+polinom = calculate_polinom_Newton(new_data, number, cur_x)
 
-print(new_difs)
+print(polinom)
 
