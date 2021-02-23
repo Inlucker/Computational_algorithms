34663a88ca3afd0ce16f8fe1d797126f0777929e
Arseny Pronin <arseny707@mail.ru>
2021-02-23 03:05:28 +0300

    completed coding lab1

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,97 +1,96 @@
-table = ((0.00, 1.000000, -1.00000),
-         (0.15, 0.838771, -1.14944),
-         (0.30, 0.655336, -1.29552),
-         (0.45, 0.450447, -1.43497),
-         (0.60, 0.225336, -1.56464),
-         (0.75,-0.018310, -1.68164),
-         (0.90,-0.278390, -1.78333),
-         (1.05,-0.552430, -1.86742))
+table1 = [(0.00, 1.000000, -1.00000),
+          (0.15, 0.838771, -1.14944),
+          (0.30, 0.655336, -1.29552),
+          (0.45, 0.450447, -1.43497),
+          (0.60, 0.225336, -1.56464),
+          (0.75,-0.018310, -1.68164),
+          (0.90,-0.278390, -1.78333),
+          (1.05,-0.552430, -1.86742)]
 
+'''
+table3 = ((0.00, 1.000000, -1.00000),
+          (0.15, 0.838771, -1.14944),
+          (0.30, 0.655336, -1.29552),
+          (0.45, 0.450447, -1.43497),
+          (0.60, 0.225336, -1.56464),
+          (0.75,-0.018310, -1.68164),
+          (0.90,-0.278390, -1.78333),
+          (1.05,-0.552430, -1.86742))
+'''
+
+'''
 table2 = ([0.00, 1.0,   1],
           [0.25, 0.924, 2],
           [0.50, 0.707, 3],
           [0.75, 0.383, 4],
           [1.0, 0.0,    5])
+'''
 
 #print (table)
 
-def make_data_Newton(x, n):
+def make_data_Newton(x, n, table):
+    n += 1
     rez = []
-    rez.append(list(table[0]))
-    index = 0;
+    rez.append(list(table[len(table) - 1]))
+    index = len(table) - 1
+    '''
     for i in range(len(table)):
         if table[i][0] <= x:
             rez[0] = list(table[i])
             index = i
+    '''
+    for i in range(1, len(table)):
+        if (x - table[i - 1][0]) * (x - table[i][0]) < 0:
+            rez[0] = list(table[i - 1])
+            index = i - 1
+    #print(rez[0])
+    #'''
 
     tmp = 1
     #print(rez)
-    for i in range(0, n - 1):
-        if (i % 2 == 0):
-            if (index + tmp < len(table)):
-                rez.append(list(table[index + tmp]))
-            elif (index - tmp >= 0):
-                rez.append(list(table[index - tmp]))
-                tmp += 1
-            else:
-                print("Not enough data")
-                rez = []
-                break
-        else:
-            if (index - tmp >= 0):
-                rez.append(list(table[index - tmp]))
-                tmp += 1
-            elif (index + tmp + 1 < len(table)):
-                tmp += 1
-                rez.append(list(table[index + tmp]))
-                tmp += 1
-            else:
-                print("Not enough data")
-                rez = []
-                break
-    '''
-    for i in range(0, n - 1):
-        if (i % 2 == 0):
-            if (index + tmp < len(table)):
-                rez.append(table[index + tmp])
-            elif (index - tmp >= 0):
-                rez.insert(0, table[index - tmp])
-                tmp += 1
-            else:
-                print("Not enough data")
-                rez = []
-                break
-        else:
-            if (index - tmp >= 0):
-                rez.insert(0, table[index - tmp])
-                tmp += 1
-            elif (index + tmp + 1 < len(table)):
-                tmp += 1
-                rez.append(table[index + tmp])
-                tmp += 1
-            else:
-                print("Not enough data")
-                rez = []
-                break
-        #print(rez)
-    '''
 
+    for i in range(0, n - 1):
+        if (i % 2 == 0):
+            if (index + tmp < len(table)):
+                rez.append(list(table[index + tmp]))
+            elif (index - tmp >= 0):
+                rez.append(list(table[index - tmp]))
+                tmp += 1
+            else:
+                print("Not enough data")
+                rez = []
+                break
+        else:
+            if (index - tmp >= 0):
+                rez.append(list(table[index - tmp]))
+                tmp += 1
+            elif (index + tmp + 1 < len(table)):
+                tmp += 1
+                rez.append(list(table[index + tmp]))
+                tmp += 1
+            else:
+                print("Not enough data")
+                rez = []
+                break
+
+    #print(rez)
     return rez
 
-def make_data_Ermit(x, n):
+def make_data_Ermit(x, n, table):
+    n += 1
     rez = []
-    rez.append(table[0])
-    index = 0;
-    for i in range(len(table)):
-        if table[i][0] <= x:
-            rez[0] = list(table[i])
-            index = i
+    rez.append(list(table[len(table) - 1]))
+    index = len(table) - 1
+    for i in range(1, len(table)):
+        if (x - table[i - 1][0]) * (x - table[i][0]) < 0:
+            rez[0] = list(table[i - 1])
+            index = i - 1
+
+    tmp = 1
+    #print(rez)
 
     counter = 1
     down = 1
-
-    tmp = 1
-    #print(rez)
 
     while (counter < n):
         #print (rez)
@@ -123,11 +122,6 @@ def make_data_Ermit(x, n):
             down = (down + 1) % 2
         counter += 1
 
-
-    #for i in range (len(rez) - 1):
-    #    if (rez[i][1] == rez[i + 1][1]):
-    #        rez[i] = [rez[i][0], rez[i][2], rez[i][2]]
-
     return rez
 
 
@@ -136,7 +130,6 @@ def get_dif(data, yi, yj, xi, xj):
         dif = data[xi][2]
     else:
         dif = (data[yi][1] - data[yj][1]) / (data[xi][0] - data[xj][0])
-    #dif = (data[yi][1] - data[yj][1]) / (data[xi][0] - data[xj][0])
     return dif
 
 def calculate_polinom(data, n, x):
@@ -150,7 +143,6 @@ def calculate_polinom(data, n, x):
             difs[j][1] = get_dif(difs, j, j + 1, j, j + 1 + i)
         tmp = difs[0][1]
         for k in range (i + 1):
-            #mn = x - difs[k][0]
             tmp *= x - difs[k][0]
         rez += tmp
         #print("Difs = ", difs)
@@ -180,9 +172,10 @@ def calculate_polinom_Ermit(data, n, x):
     return rez
 '''
 
-cur_x = 0.525
+#cur_x = 0.525
+#cur_x = 1.05
 cur_x = float(input("Введите x: "))
-print()
+print("x =", cur_x)
 
 print ("|-------------------------------|")
 print ("| Степень |  Ньютон  |  Эрмит   |")
@@ -192,9 +185,7 @@ for i in range(1, 5):
     #print("Степень =", stepen)
     print("|{:^9d}".format(stepen), end = "")
 
-    new_data_Newton = make_data_Newton(cur_x, stepen + 1)
-
-    new_data_Ermit = make_data_Ermit(cur_x, stepen + 1)
+    new_data_Newton = make_data_Newton(cur_x, stepen, table1)
 
     #print(new_data_Newton)
 
@@ -205,7 +196,7 @@ for i in range(1, 5):
         else:
             print("|{:^8.6f} |".format(polinom_Newton), end = "")
 
-
+    new_data_Ermit = make_data_Ermit(cur_x, stepen, table1)
     #print(new_data_Ermit)
 
     if (not new_data_Ermit == []):
@@ -215,5 +206,33 @@ for i in range(1, 5):
         else:
             print("{:^8.6f} |".format(polinom_Newton), end = "")
 
-
     print("\n|-------------------------------|")
+
+cur_y = 0
+
+inverse_table1 = []
+for i in range(len(table1)):
+    #inverse_table1.insert(0, (table1[i][1], table1[i][0]))
+    inverse_table1.append((table1[i][1], table1[i][0]))
+#print(inverse_table1)
+
+print ("|--------------------|")
+print ("| Степень |  Корень  |")
+print ("|--------------------|")
+for i in range(1, 5):
+    stepen = i
+    #print("Степень =", stepen)
+    print("|{:^9d}".format(stepen), end = "")
+
+    new_data_Newton = make_data_Newton(cur_y, stepen, inverse_table1)
+
+    #print(new_data_Newton)
+
+    if (not new_data_Newton == []):
+        polinom_Newton = calculate_polinom(new_data_Newton, stepen, cur_y)
+        if (polinom_Newton > 0):
+            print("| {:^8.6f} |".format(polinom_Newton), end = "")
+        else:
+            print("|{:^8.6f} |".format(polinom_Newton), end = "")
+
+    print("\n|--------------------|")
